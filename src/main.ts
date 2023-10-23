4f18af394384be9dfd379f6ce7af5840729b5439
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-23 21:26:45 +0900

    init implementation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from "@actions/core";
-import { wait } from "./wait";
+import { runCommand } from "./command";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds");
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
-
-    core.setOutput("time", new Date().toTimeString());
+    const shell = core.getInput("shell");
+    const inputRun = core.getInput("run");
+    const workingDirectory = core.getInput("working-directory");
+    await runCommand({
+      shell,
+      run: inputRun,
+      workingDirectory,
+    });
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
